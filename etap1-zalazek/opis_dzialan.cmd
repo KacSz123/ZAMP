e466d4605be22ce7c56469b6c852abf9ad46d625
KacSz123 <kacperszcz159@gmail.com>
2021-12-07 23:27:08 +0100

    rozroznianie komendy begin/end_parallel_actions

--- etap1-zalazek/opis_dzialan.cmd
+++ etap1-zalazek/opis_dzialan.cmd
@@ -10,7 +10,9 @@ Rotate Ob_A.Ob_B ROTATE_SPEED Z 40
 Rotate Ob_A ROTATE_SPEED Z 40
 Pause Ob_A 1000 /*Zawieszenie na 1 sek. */
 Move Ob_A 10 10 
+Begin_Parallel_Actions
 Rotate Ob_A ROTATE_SPEED Z 40/* Rotate i Move wykonywane razem */
 Move Ob_A.Ob_B  10 10   
 Rotate Ob_A.Ob_B.Ramie2 ROTATE_SPEED Z 40   
 Move Ob_A.Ob_B.Ramie2  10 10    /* powoduje jazde po luku */
+End_Parallel_Actions
